a335825c8e7dc4bf518fde52e64a9cf5895edf49
yamaksy287 <yamaksy287@mail.ru>
2023-11-26 15:27:50 +0300

    add registration and login

--- server/src/main/kotlin/com/euproff/plugins/Routing.kt
+++ server/src/main/kotlin/com/euproff/plugins/Routing.kt
@@ -6,16 +6,103 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Test(
-    val text: String
+data class LoginReceiveRemote(
+    val login: String,
+    val password: String
 )
+
+@Serializable
+data class LoginResponseRemote(
+    val token: String
+)
+@Serializable
+data class RegisterReceiveRemote(
+    val login: String,
+    val email: String,
+    val password: String
+)
+
+@Serializable
+data class RegisterResponseRemote(
+    val token: String
+)
+class RegisterController(private val call: ApplicationCall) {
+
+    suspend fun registerNewUser() {
+        val registerReceiveRemote = call.receive<RegisterReceiveRemote>()
+        if (!registerReceiveRemote.email.isValidEmail()) {
+            call.respond(HttpStatusCode.BadRequest, "Email is not valid")
+        }
+
+        val userDTO = Users.fetchUser(registerReceiveRemote.login)
+        if (userDTO != null) {
+            call.respond(HttpStatusCode.Conflict, "User already exists")
+        } else {
+            val token = UUID.randomUUID().toString()
+
+            try {
+                Users.insert(
+                    UserDTO(
+                        login = registerReceiveRemote.login,
+                        password = registerReceiveRemote.password,
+                        email = registerReceiveRemote.email,
+                        username = ""
+                    )
+                )
+            } catch (e: ExposedSQLException) {
+                call.respond(HttpStatusCode.Conflict, "User already exists")
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest, "Can't create user ${e.localizedMessage}")
+            }
+
+            Tokens.insert(
+                TokenDTO(
+                    rowId = UUID.randomUUID().toString(), login = registerReceiveRemote.login,
+                    token = token
+                )
+            )
+
+            call.respond(RegisterResponseRemote(token = token))
+        }
+    }
+}
+class LoginController(private val call: ApplicationCall) {
+
+    suspend fun performLogin() {
+        val receive = call.receive<LoginReceiveRemote>()
+        val userDTO = Users.fetchUser(receive.login)
+
+        if (userDTO == null) {
+            call.respond(HttpStatusCode.BadRequest, "User not found")
+        } else {
+            if (userDTO.password == receive.password) {
+                val token = UUID.randomUUID().toString()
+                Tokens.insert(
+                    TokenDTO(
+                        rowId = UUID.randomUUID().toString(), login = receive.login,
+                        token = token
+                    )
+                )
+
+                call.respond(LoginResponseRemote(token = token))
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "Invalid password")
+            }
+        }
+    }
+}
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respond(Test(text="hello world"))
         }
-        post("/login"){
-
+        post("/login") {
+            val loginController = LoginController(call)
+            loginController.performLogin()
+        }
+        post("/register") {
+            val registerController = RegisterController(call)
+            registerController.registerNewUser()
         }
     }
 }
